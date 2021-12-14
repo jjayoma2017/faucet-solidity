c27c4a0282634a9f53c95ce03763c88b923acc84
jjayoma2017 <jjayoma2017@gmail.com>
2021-12-14 14:19:49 +0800

    added connect button

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
       const provider = await detectEthereumProvider()
 
       if (provider) {
-        provider.request({ method: 'eth_requestAccounts' })
         setWeb3Api({
           web3: new Web3(provider),
           provider,
@@ -35,20 +34,34 @@ function App() {
     }
     web3Api.web3 && getAccounts()
   }, [web3Api.web3])
-
+  // provider.request({ method: 'eth_requestAccounts' })
   return (
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
-          <span>
-            <strong>Account: </strong>
-          </span>
-          <h1>{account ? account : 'not connected'}</h1>
-          <div className="balance-view is-size-2">
+          <div className="is-flex align-items-center">
+            <span>
+              <strong className="mr-2">Account: </strong>
+            </span>
+
+            {account ? (
+              <span> {account}</span>
+            ) : (
+              <button
+                className="button is-small  mr-2"
+                onClick={() =>
+                  web3Api.provider.request({ method: 'eth_requestAccounts' })
+                }
+              >
+                Connect Wallet
+              </button>
+            )}
+          </div>
+          <div className="balance-view is-size-2  my-4">
             Current Balance: <strong>10</strong> ETH
           </div>
-          <button className="button mr-2">Donate</button>
-          <button className="button">Withdraw</button>
+          <button className="button is-primary is-light mr-2">Donate</button>
+          <button className="button is-link is-light">Withdraw</button>
         </div>
       </div>
     </>
