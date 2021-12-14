cb6f5ac530c91951282037b0aec9830e03688f4c
jjayoma2017 <jjayoma2017@gmail.com>
2021-12-14 13:54:04 +0800

    added providers and metamask

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
-import logo from './logo.svg'
+import './App.css'
+import { useEffect, useState } from 'react'
+import Web3 from 'web3'
+import detectEthereumProvider from '@metamask/detect-provider'
 
 function App() {
-  return <div className="App">Hello World</div>
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+  })
+
+  const [account, setAccount] = useState(null)
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider()
+
+      if (provider) {
+        provider.request({ method: 'eth_requestAccounts' })
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider,
+        })
+      } else {
+        console.error('Please install metamask')
+      }
+    }
+
+    loadProvider()
+  }, [])
+
+  useEffect(() => {
+    const getAccounts = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts()
+      setAccount(accounts[0])
+    }
+    web3Api.web3 && getAccounts()
+  }, [web3Api.web3])
+
+  return (
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <span>
+            <strong>Account: </strong>
+          </span>
+          <h1>{account ? account : 'not connected'}</h1>
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button className="button mr-2">Donate</button>
+          <button className="button">Withdraw</button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App
